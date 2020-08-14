f31e83203a17ed5cf9a9fa3153721a6de7c61fc3
vijay sagar gooty <31399807+vijaysagar111@users.noreply.github.com>
2020-08-14 08:44:19 +0530

    Added Level Ups

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+var maindiv = document.createElement("div");
+document.body.appendChild(maindiv);
+maindiv.className = "container";
+
+var p = document.createElement("p");
+maindiv.appendChild(p);
+p.id = "refresh";
 
 var div = document.createElement("div");
-document.body.appendChild(div);
+maindiv.appendChild(div);
 
 var h1 = document.createElement("h1");
 div.appendChild(h1);
@@ -9,6 +16,10 @@ h1.innerText = "Crack the CODE";
 
 var div1 = document.createElement("div");
 div.appendChild(div1)
+
+var para = document.createElement("p");
+div1.appendChild(para);
+para.id = "level";
 
 var inp = document.createElement("input");
 inp.type = "number";
@@ -21,12 +32,14 @@ var br = document.createElement("br");
 div.appendChild(br);
 
 var bt = document.createElement("button");
-bt.id= "submit";
+bt.id = "submit";
 bt.innerText = "Submit";
 div1.appendChild(bt);
 bt.onclick = "getInput()"
 
-
+p1 = document.createElement("p");
+div.appendChild(p1);
+p1.id = "alert";
 
 var div2 = document.createElement("div");
 div.appendChild(div2)
@@ -37,6 +50,12 @@ div2.innerHTML = ` <p id="o_p"></p>
 
 var div3 = document.createElement("div");
 div.appendChild(div3)
+var para = document.createElement("p");
+div3.appendChild(para);
+para.id = "lossed";
+var para = document.createElement("p");
+div3.appendChild(para);
+para.id = "score";
 
 ul = document.createElement("ul");
 ul.id = "step";
@@ -46,132 +65,183 @@ div3.appendChild(ul);
 
 li = document.createElement("li");
 li.innerHTML = "<b> Results </b>";
-ul.append(li);
+ul.appendChild(li);
+
 
 
 var div4 = document.createElement("div");
 div.appendChild(div4)
+div4.className = "greencolor";
 div4.innerHTML = `Note:<br>
-'Y' Indicates a number is in the correct position.
- <br>'E' Indicates a number is part of the code, but not in the right position.
- <br>'E' Doesn't consider how many times a number exists in the code.
-<br>'X' Indicates a number is not part of the code.`;
+'Green' Indicates a number is in the correct position.
+ <br>'Orange' Indicates a number is part of the code, but not in the right position.
+ <br>'Orange' Doesn't consider how many times a number exists in the code.
+<br>'Red' Indicates a number is not part of the code.`;
 
-
-
+l = 0;
+var size = sessionStorage.getItem("1");
+var s = sessionStorage.getItem("br");
+if (size <= 0) {
+  document.getElementById("refresh").innerText = "Please Refresh your  browser!!!!!"
+  l = 0;
+  size = sessionStorage.setItem("1", 4);
   
+}
+
+
+size = parseInt(size);
+
+
 var secret_code = generateCode();
-      var step_counter = 0;
-      document.getElementById("submit").addEventListener("click", getInput);
+var step_counter = 0;
+document.getElementById("submit").addEventListener("click", getInput);
 
 
-      function getInput() {
-        var x = document.getElementById('i_p').value;
-        console.log(x)
-        processInput(x);
-      } 
+function getInput() {
+  var x = document.getElementById('i_p').value;
+  console.log(x)
+  processInput(x);
+}
 
-      function processInput(input) {
-        var n = input.length;
-        if (document.getElementById("submit").innerHTML === "Reset") {
-          cleanAll();
-          return;
-        }
-        else if (n > 4) {
-          o_p.innerHTML = "Input exceeds 4 character!";
-          return;
-        }
-        else if (n < 4) {
-          o_p.innerHTML = "Input is less than 4 character!";
-          return;
-        }
-        else if (step_counter === 5) {
-          document.getElementById("is_game").innerHTML = "You lose!! ";
-          resetGame();
-          return;
-        }
-        else {
-          step_counter++;
-          checkSubmission(input);
-          return;
-        }
+function getRandom(min, max) {
 
-        return;
-      }
+  var random = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(random);
+  return random;
+}
 
-      function generateCode() {
-        var code = "";
-        for (var i = 0; i < 4; i++) {
-          var n = getRandom(0, 9);
-          code += n.toString();
-        }
-        return code;
-      }
+function processInput(input) {
+  document.getElementById("alert").innerHTML = `Please Enter ${size} numbers.`;
+  var n = input.length;
+  if (document.getElementById("submit").innerHTML === "Reset") {
+    cleanAll();
+    return;
+  }
+  else if (n > size) {
+    o_p.innerHTML = `Input exceeds ${size} digits!`;
+    return;
+  }
+  else if (n < size) {
+    o_p.innerHTML = `Input is less than ${size} digits!`;
+    return;
+  }
+  else if (step_counter === 5) {
 
-      function getRandom(min, max) {
-        
-        var random = Math.floor(Math.random() * (max - min + 1)) + min;
-        console.log(random);
-        return random;
-      }
+    document.getElementById("is_game").innerHTML = "You lose!! ";
+    document.getElementById("lossed").innerText = `The number is ${generateCode()}. Cheer up! Try again!!!`
+    resetGame();
+    return;
+  }
+  else {
+    step_counter++;
+    checkSubmission(input);
+    return;
+  }
 
-      function checkSubmission(usr_input) {
-        
-        if (usr_input === secret_code) {
-          document.getElementById("is_game").innerHTML = "Cool! You Win!";
-          resetGame();
-        }
-        var result = "";
+  return;
+}
 
-        for (var i = 0; i < 4; i++) {
-          var found = false;
-          if (usr_input[i] === secret_code[i]) { 
-            
-           v1 = usr_input[i];
-           
-        
-            result += "Y ";
-            
-            found = true;
-            continue;
-          }
-          for (var j = 0; j < 4; j++) {
-            if (usr_input[i] === secret_code[j]) {
-              result += "E "
-              found = true;
-              break;
-            }
-          }
-          if (!found) {
-            result += "X ";
-          }
-        }
-        //document.getElementById("check").innerHTML = result;
-        showSubmission(result, usr_input);
-        return;
-      }
-      function showSubmission(result, usr_input) {
-        
+function generateCode() {
+  var code = "";
+  for (var i = 0; i < size; i++) {
+    var n = getRandom(0, 9);
+    code += n.toString();
+  }
+  return code;
+}
+
+
+
+
+
+
+function checkSubmission(usr_input) {
+
+
+  if (usr_input === secret_code) {
+    document.getElementById("is_game").innerHTML = "Cool! You Win!";
+    document.getElementById("level").innerText = `Level ${l++} Passed`;
+    document.getElementById("score").innerText = `Score: ${size}`;
+    sessionStorage.setItem("1", size++);
+    resetGame();
+
+  }
+  var result = "";
+  
+  for (var i = 0; i < size; i++) {
+    var found = false;
+    if (usr_input[i] === secret_code[i]) {
+
+
+
+
+      result = "Y"
+      //showSubmission(result, usr_input);
+      var ul = document.getElementById("step");
+      var li = document.createElement("li");
+      
+      li.classList = "circles1";
+      li.appendChild(document.createTextNode(result));
+      ul.appendChild(li);
+
+      found = true;
+      continue;
+      //continue;
+    }
+   
+    for (var j = 0; j < size; j++) {
+      if (usr_input[i] === secret_code[j]) {
+        result = "E"
         var ul = document.getElementById("step");
         var li = document.createElement("li");
-
+        //var len = ul.getElementsByTagName("li").length;
         
-       
+        li.classList = "circles2";
         li.appendChild(document.createTextNode(result));
         ul.appendChild(li);
+        found = true;
+        //showSubmission(result, usr_input);
+        break;
       }
+    }
+    if (!found) {
+      result = "X"
+      var ul = document.getElementById("step");
+      var li = document.createElement("li");
+      
+      li.classList = "circles3";
+      li.appendChild(document.createTextNode(result));
+      ul.appendChild(li);
+      //showSubmission(result, usr_input);
+    }
+  }
+  //document.getElementById("check").innerHTML = result;
+  //showSubmission(result, usr_input);
+  return;
+}
+function showSubmission(result, usr_input) {
 
-      function resetGame() {
-        document.getElementById("submit").innerHTML = "Reset";
-      }
+  var ul = document.getElementById("step");
+  var li = document.createElement("li");
+  li.classList = "circles1";
+  li.appendChild(document.createTextNode(result));
+  ul.appendChild(li);
 
-      function cleanAll() {
-        secret_code = generateCode();
-        step_counter = 0;
-        document.getElementById("o_p").innerHTML = "";
-        document.getElementById("is_game").innerHTML = "Keep Guessing!!";
-        document.getElementById("step").innerHTML = "<li><b>Result</b></li>";
-        document.getElementById("submit").innerHTML = "Submit";
-        
-        return;
-      }
+}
+
+function resetGame() {
+  document.getElementById("submit").innerHTML = "Reset";
+}
+
+function cleanAll() {
+  secret_code = generateCode();
+  step_counter = 0;
+  document.getElementById("i_p").value = "";
+  document.getElementById("o_p").innerText = "";
+  document.getElementById("lossed").innerText = "";
+  document.getElementById("is_game").innerHTML = "Keep Guessing!!";
+  document.getElementById("step").innerHTML = "<li><b>Result</b></li>";
+  document.getElementById("submit").innerHTML = "Submit";
+
+  return;
+}
